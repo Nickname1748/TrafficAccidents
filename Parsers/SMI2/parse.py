4b2cb603212fe9160c38df24c322800834b5ca0c
MilkyCode13 <shmayhel.andrey@gmail.com>
2019-11-11 19:55:01 +0300

    Added time check to SMI2 parser + fixed quotation marks

--- Parsers/SMI2/parse.py
+++ Parsers/SMI2/parse.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import httplib2
+import time
 
 all_news = []
 KEY_WORDS = ['ДТП', 'сбили%20пешехода']
@@ -13,14 +14,15 @@ class News():
 def parse(url):
     jsonurl = urllib.request.urlopen(url)
     obj = json.load(jsonurl)
-    articles = obj["articles"]
+    articles = obj['articles']
     for art in articles:
+        if(int(art['create_date']) < time.time() - 86400):
+            continue
         news = News()
-        news.title = art["title_original"]
-        news.article = art["announce_original"]
-        news.link = art["share_url"]
+        news.title = art['title_original']
+        news.article = art['announce_original']
+        news.link = art['share_url']
         all_news.append(news)
 
 for word in KEY_WORDS:
     parse(httplib2.iri2uri('https://smi2.ru/api/search?limit=100&offset=0&order=date&query={}'.format(word)))
-    
