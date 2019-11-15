076f5616506773b89bc54f7435c76d2518a2a5c8
MilkyCode13 <shmayhel.andrey@gmail.com>
2019-11-15 20:18:15 +0300

    Refactored Google Alerts parser

--- Parsers/GoogleAlerts/parse.py
+++ Parsers/GoogleAlerts/parse.py
@@ -2,24 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 all_news = []
-
-class News():
-	title = ""
-	article = ""
-	link = ""
+KEY_WORDS = ['дтп', 'пешеход', 'сбили%20пешехода']
 
 def parse(lxml): 
 	request = requests.get(lxml)
 	website = BeautifulSoup(request.text, 'html.parser')
 	page = website.findAll('li', 'result')
 	for n in range(len(page)):
-		news = News()
+		news = {'title': '', 'article': '', 'link': ''}
 		if (page[n].find('img') != None):
-			news.title = page[n].find('h4').text
+			news['title'] = page[n].find('h4').text
 		else:
-			news.title= page[n].find('a').text
-		news.article = page[n].find('span').text
-		news.link = page[n].find('a', 'result_title_link')['href']
+			news['title'] = page[n].find('a').text
+		news['article'] = page[n].find('span').text
+		news['link'] = page[n].find('a', 'result_title_link')['href']
 		all_news.append(news)		
 
-parse('https://www.google.ru/alerts/preview?params=[null,[null,null,null,[null,"\u0434\u0442\u043f","ru",[null,"ru","RU"],null,null,null,0,1],null,3,[[null,1,"user@example.com",[null,null,10],2,"ru-RU",null,null,null,null,null,"0",null,null,"AB2Xq4hcilCERh73EFWJVHXx-io2lhh1EhC8UD8"]]],0]')
+for word in KEY_WORDS:
+	parse('https://www.google.ru/alerts/preview?params=[null,[null,null,null,[null,"{}","ru",[null,"ru","RU"],null,null,null,0,1],null,3,[[null,1,"user@example.com",[null,null,10],2,"ru-RU",null,null,null,null,null,"0",null,null,"AB2Xq4hcilCERh73EFWJVHXx-io2lhh1EhC8UD8"]]],0]'.format(word))
+print(all_news)
