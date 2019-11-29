d9281ee22c9c07bf43c1bb69b5fa6cbccf354362
MilkyCode13 <shmayhel.andrey@gmail.com>
2019-11-29 21:21:09 +0300

    Added basic email layout

---
+++ LayoutAndSend/LayoutEmail.py
@@ -0,0 +1,19 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
+import locale
+
+def layout(all_news):
+    locale.setlocale(locale.LC_ALL, 'ru')
+    msg = MIMEMultipart()
+    msg['Subject'] = "Новости о ДТП за {}".format(time.strftime("%e %B %Y года", time.gmtime(time.time())))
+    # Not implemented yet
+    #msg['From'] = 
+    #msg['To'] = 
+    text = "За {} мы собрали следующие новости:"
+    for news in all_news:
+        text = text + '\n\n' + news['title'] + '\n' + news['link'] + '\n' + news['article']
+    textpart = MIMEText(text, 'plain')
+    # TODO: HTML part
+    msg.attach(textpart)
+    return msg
