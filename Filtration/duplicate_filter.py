a0e6beab993d5c5eb8d31989d79d6ab1d0c41b78
MilkyCode13 <shmayhel.andrey@gmail.com>
2020-01-19 19:34:43 +0300

    Fixed duplicate filter and added to main

--- Filtration/duplicate_filter.py
+++ Filtration/duplicate_filter.py
@@ -6,6 +6,17 @@ import re
 from sklearn.feature_extraction.text import TfidfVectorizer # Machine learning
 from sklearn.metrics.pairwise import cosine_distances
 
+def remove_same(news):
+    news.sort(key=lambda n: n['title'])
+    i = 0
+    while i < len(news) - 1:
+        if news[i]['title'] == news[i+1]['title']:
+            del news[i+1]
+            continue
+        else:
+            i = i + 1
+    return news
+
 def lemmatization(list_of_strings):
     morph = MorphAnalyzer()
     for i in range(len(list_of_strings)):
@@ -31,28 +42,34 @@ def compare_cosine_distances(vectors):
     return result
 
 def find_similar(distances):
+    distsformed = []
+    for i in range(len(distances)):
+        for k in range(i+1, len(distances[i])):
+            distsformed.append([distances[i][k], i, k])
+    distsformed.sort(key=lambda d: d[0])
+    distsformed = filter(lambda d: d[0] < 0.6, distsformed) #THIS NUMBER SHOULD BE TESTED
     sets = []
     setid = [-1]*len(distances) # Every item is not a member of any set
-    for i in range(len(distances)):
-        for k in range(len(distances[i])):
-            if k != i:
-                if distances[i][k] < 0.6: #THIS NUMBER SHOULD BE TESTED
-                    if setid[i] != -1:
-                        if setid[k] == -1:
-                            sets[setid[i]].add(k)
-                            setid[k] = setid[i]
-                    elif setid[k] != -1:
-                        if setid[i] == -1:
-                            sets[setid[k]].add(i)
-                            setid[i] = setid[k]
-                    else:
-                        setid[i] = len(sets)
-                        setid[k] = len(sets)
-                        sets.append(set([i,k]))
-    sets.sort(key=lambda s: len(s))
+    for dist in distsformed:
+        i = dist[1]
+        k = dist[2]
+        if setid[i] != -1:
+            if setid[k] == -1:
+                sets[setid[i]].add(k)
+                setid[k] = setid[i]
+        elif setid[k] != -1:
+            if setid[i] == -1:
+                sets[setid[k]].add(i)
+                setid[i] = setid[k]
+        else:
+            setid[i] = len(sets)
+            setid[k] = len(sets)
+            sets.append(set([i,k]))
+    sets.sort(key=lambda s: len(s), reverse=True)
     return sets
 
 def main_filter(news):
+    news = remove_same(news) # Remove exact same news
     list_of_strings = get_list_of_strings(news)
     list_of_strings = clear_corpus(list_of_strings) # Remove unneeded words
     list_of_strings = lemmatization(list_of_strings)
@@ -60,10 +77,14 @@ def main_filter(news):
     distances = compare_cosine_distances(vectors)
     sets = find_similar(distances)
     finalnews = [] # Put all news to groups
+    grouped = set()
     for s in sets:
         newsgroup = []
         for i in s:
-            newsgroup.append(news.pop(i))
+            newsgroup.append(news[i])
+            grouped.add(i)
         finalnews.append(newsgroup)
-    finalnews.extend(news) # Add remained news without groups
+    for i in range(len(news)):
+        if i not in grouped:
+            finalnews.append(news[i]) # Add remained news without groups
     return finalnews
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart
 
 import Parsers.Parse_all as parse
 import Filtration.location as location
+import Filtration.duplicate_filter as duplicate_filter
 import LayoutAndSend.LayoutEmail as layout
 import LayoutAndSend.SendEmail as send
 
@@ -14,6 +15,7 @@ KEY_WORDS = ['дтп', 'пешеход', 'сбили%20пешехода']
 
 all_news = parse.main(KEY_WORDS)
 all_news = location.location_filter(all_news)
+all_news = duplicate_filter.main_filter(all_news)
 if "--nosend" not in args:
     msg = layout.layout(all_news)
     send.send(msg)
