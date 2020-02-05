08a97cdf2ee373a03e463ca8a9961241a71f51bf
MilkyCode13 <shmayhel.andrey@gmail.com>
2020-02-05 19:40:20 +0300

    Improved test script for text tone analysis

--- Test/GetTone.py
+++ Test/GetTone.py
@@ -9,5 +9,8 @@ KEY_WORDS = ['дтп', 'пешеход', 'сбили%20пешехода']
 
 all_news = parse.main(KEY_WORDS)
 all_news = ToneAnalysis.analyze(all_news)
+all_news.sort(key=lambda news: news['tone']['negative'])
+print("{:>8} {:>8} {:>8} {}".format('Positive', 'Neutral', 'Negative', 'Article'))
 for news in all_news:
-    print(news['title']+'\n'+news['article']+'\n'+str(news['tone'])+'\n\n')
+    # print(news['title']+'\n'+news['article']+'\n'+str(news['tone'])+'\n\n')
+    print("{:.6f} {:.6f} {:.6f} {}".format(news['tone']['positive'], news['tone']['neutral'], news['tone']['negative'], news['title']))
